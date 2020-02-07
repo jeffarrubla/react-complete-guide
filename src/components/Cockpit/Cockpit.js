2e4d2dcf2821176ec9fd4d2148dcbc9e77c521cf
Jefferson <jeffarrubla@gmail.com>
2020-02-07 16:10:11 +0000

    Using shouldComponentUpdate for optimization

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,11 +5,10 @@ const cockpit = (props) => {
 	useEffect(() => {
 		console.log('[Cockpit.js] useEffect');
 		// Http request
-		const timer = setTimeout(() => {
+		setTimeout(() => {
 			alert('Saved date from cloud');
 		}, 1000);
 		return () => {
-			clearTimeout(timer);
 			console.log('[Cockpit.js] clean up work with useEffect')
 		}
 	}, []); //only runs the 1st time
