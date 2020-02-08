36d42ca34f7c84022f806d23b8013f36ea0f7928
Jefferson <jeffarrubla@gmail.com>
2020-02-08 20:48:56 +0000

    Using the context API

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
 	const toggleBtnRef = useRef(null);	
@@ -44,9 +45,11 @@ const cockpit = (props) => {
 		        onClick={props.clicked}>
 	            Toggle Persons        
 	        </button>
-	        <button onClick={props.login}>
-	        	log in
-	        </button>
+	        <AuthContext.Consumer>
+		        {context => <button onClick={context.login}>
+		        	log in
+		        </button>}
+	        </AuthContext.Consumer>
 	    </div>
 	);
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 import withClass from '../hoc/withClass';
-import Aux from '../hoc/Aux'
+import Aux from '../hoc/Aux';
+import AuthContext from '../context/auth-context';
 
 class App extends Component {
   constructor(props) {
@@ -105,15 +106,21 @@ class App extends Component {
         <button onClick={() => {
             this.setState({ showCockpit: false });
           }} >Remove Cockpit</button>
-          {this.state.showCockpit ? 
-          <Cockpit 
-            title={this.props.appTitle}
-            showPersons={this.state.showPersons} 
-            personsLength={this.state.persons.length}
-            clicked={this.togglePersonsHandler} 
-            login={this.loginHandler}
-            />: null }
-          {persons}
+          <AuthContext.Provider 
+            value={{
+              authenticated: this.state.authenticated, 
+              login: this.loginHandler
+            }}>
+            {this.state.showCockpit ? 
+            <Cockpit 
+              title={this.props.appTitle}
+              showPersons={this.state.showPersons} 
+              personsLength={this.state.persons.length}
+              clicked={this.togglePersonsHandler} 
+              login={this.loginHandler}
+              />: null }
+            {persons}
+          </AuthContext.Provider>
       </Aux>
     )
   }
